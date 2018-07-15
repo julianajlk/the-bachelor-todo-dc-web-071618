2c7c3d11663a2fda610519fa18364352f641ed44
Juliana Kang <julianajlk@gmail.com>
2018-07-15 00:34:34 +0000

    automatically backed up by learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -50,7 +50,7 @@ def get_average_age_for_season(data, season)
       contestant.each do |age, i|
         if age == "age"
           count += 1
-          
+        end
       end
     end
     (age/counter).round
